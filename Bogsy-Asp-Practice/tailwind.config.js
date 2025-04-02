e0c6c250e7adebea7225cb94db612bba23817e81
LetAdrianCook <adrianvillasis2252@gmail.com>
2025-04-02 14:23:12 +0800

    Add project files.

---
+++ Bogsy-Asp-Practice/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/**/*.cshtml",
+        "./Views/**/*.cshtml",
+        "./wwwroot/**/*.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
